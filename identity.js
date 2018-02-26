99c37e0022c9d934adeec65f4902ed83a11bdf26
Dustin McCaffree <dustin@smartystreets.com>
2018-02-26 13:06:18 -0700

    Got the actual clientId...

--- identity.js
+++ identity.js
@@ -1,5 +1,5 @@
 (() => {
-	const clientId = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzC2WGW9HXEL44NpgIbmYvQmKW60ZNQ4eTnCGcVWQqbZlpBfa9FaHW/3aPlkGfgy0xOvrlUCDd8qC8TF9mSiTVE9EwHTrlIuw/4YYL5U9aXbh92cqxB6t+zyZLMDm7qj5RLTeTMiAGAQ7rnsW11uGw7PeQVOnnQIKk+N/JgSF9ToZ1OglOiuorBHLYjhRjBKDN/tkvGWtwm10UwYdF9H//z72ABdWyaMXGNTIibzOEJzUXAD9WWZcdhRTohtcPt+YS+uOfAnsdYoPxUVzPBHg8UxxjHTmaZUl0g9a+5iSKv6yiAGjkYQg/I1TVd86LoDqgSs2f4L9FFYRs6mb/7GvjwIDAQAB";
+	const clientId = "hemjflepggljigpcaneoeldgipbpcbmg";
 
 	chrome.runtime.sendMessage(clientId, {context: "external", type: "identity"}, userProfile => {
 		console.log(userProfile);
