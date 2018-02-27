9ef3d088c022d1257a40f91e945e537b50e6afb8
Dustin McCaffree <dustin@smartystreets.com>
2018-02-27 07:10:33 -0700

    Fixed timestamp href, further formatting.

--- all-notes.js
+++ all-notes.js
@@ -5,7 +5,10 @@
 		notes.slice().reverse().map(({timestamp, createdAt, videoId, content, tags}) => {
 			const thumbnailUrl = getVideoThumbnailUrl(videoId);
 			let newNote = $(".note").first().clone();
-			newNote.find(".note-timestamp").text(formatTimestamp(timestamp)).attr("href", getTimestampedUrl(timestamp, videoId));
+			newNote.find(".note-timestamp").text(formatTimestamp(timestamp)).attr({
+				href: getTimestampedUrl(timestamp, videoId),
+				target: "_blank",
+			});
 			newNote.find(".note-content").text(content.trunc(40));
 			newNote.find(".video-thumbnail").append($(document.createElement("img")).attr("src", thumbnailUrl));
 			newNote.find(".note-created-at").text(moment(createdAt).fromNow());
@@ -25,7 +28,7 @@
 	};
 
 	const getTimestampedUrl = (timestamp, videoId) => {
-		return "/watch?v=" + videoId + "&t=" + timestamp + "s";
+		return "https://youtube.com/watch?v=" + videoId + "&t=" + timestamp + "s";
 	};
 
 	String.prototype.trunc = function(n) {
