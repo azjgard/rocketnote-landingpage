e8c20318d06444a8b187f6cccabc18780ac00935
Dustin McCaffree <dustin@smartystreets.com>
2018-02-27 07:04:33 -0700

    Added formatting to notes.

--- all-notes.js
+++ all-notes.js
@@ -5,10 +5,10 @@
 		notes.slice().reverse().map(({timestamp, createdAt, videoId, content, tags}) => {
 			const thumbnailUrl = getVideoThumbnailUrl(videoId);
 			let newNote = $(".note").first().clone();
-			newNote.find(".note-timestamp").text(timestamp);
-			newNote.find(".note-content").text(content);
+			newNote.find(".note-timestamp").text(formatTimestamp(timestamp)).attr("href", getTimestampedUrl(timestamp, videoId));
+			newNote.find(".note-content").text(content.trunc(40));
 			newNote.find(".video-thumbnail").append($(document.createElement("img")).attr("src", thumbnailUrl));
-			newNote.find(".note-created-at").text(createdAt);
+			newNote.find(".note-created-at").text(moment(createdAt).fromNow());
 			newNote.find(".note-tags").text(tags);
 			$("#all-notes").append(newNote);
 		});
@@ -19,4 +19,16 @@
 	function getVideoThumbnailUrl(videoId) {
 		return "https://i1.ytimg.com/vi/" + videoId + "/mqdefault.jpg";
 	}
+
+	const formatTimestamp = timestamp => {
+		return String(moment.utc(timestamp * 1000).format('mm:ss'));
+	};
+
+	const getTimestampedUrl = (timestamp, videoId) => {
+		return "/watch?v=" + videoId + "&t=" + timestamp + "s";
+	};
+
+	String.prototype.trunc = function(n) {
+		return (this.length > n) ? this.substr(0, n-1) + '...' : this;
+	};
 })();
