bf7c7b1e22576e3d22690410b373086d036818c9
Dustin McCaffree <dustin@smartystreets.com>
2018-02-27 13:16:04 -0700

    Make hashtags filterable.

--- all-notes.js
+++ all-notes.js
@@ -110,7 +110,6 @@
 
 	function watchTagsForFilter() {
 		$(document).on("click", ".rn_tags", e => {
-			$("#search-filter").val($(e.target).text()).keyup();
 			console.log($(e.target).text());
 		});
 	}
