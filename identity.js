c8f1cd4d5d7a505ed13a1671ee4a79ba78dbac29
Dustin McCaffree <dustin@smartystreets.com>
2018-02-27 09:09:59 -0700

    Add logout button to nav.

--- identity.js
+++ identity.js
@@ -5,14 +5,24 @@
 		if (userProfile.email) {
 			convertfox.identify(userProfile.email);
 			Cookies.set('auth', 'true');
+			$("#logout-button").show();
+			$("#login-button").hide();
 		} else {
 			Cookies.set('auth', 'false');
+			$("#login-button").show();
+			$("#logout-button").hide();
 		}
 	});
 
 	$(document).on("click", "#login-button", () => {
 		chrome.runtime.sendMessage(clientId, {context: "external", type: "login"}, isLoggedIn => {
-			Cookies.set("auth", isLoggedIn ? "true" : "false")
+			Cookies.set("auth", isLoggedIn ? "true" : "false");
+			window.location.reload("/notes");
 		});
+	});
+
+	$(document).on("click", "#logout-button", () => {
+		Cookies.set("auth", "false");
+		window.location.reload();
 	});
 })();
