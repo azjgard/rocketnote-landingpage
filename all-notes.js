56e5d1a5ebc663750182f713bb4250306703e9d5
Dustin McCaffree <dustin@smartystreets.com>
2018-02-26 22:21:14 -0700

    Started mapping over available notes.

--- all-notes.js
+++ all-notes.js
@@ -2,6 +2,18 @@
 	const clientId = "hemjflepggljigpcaneoeldgipbpcbmg";
 
 	chrome.runtime.sendMessage(clientId, {context: "external", type: "getNotes"}, notes => {
-		console.log(notes);
+		notes.map(({timestamp, createdAt, videoId, content, tags}) => {
+			const thumbnailUrl = getVideoThumbnailUrl(videoId);
+			let newNote = $(".note")[0].clone();
+			newNote.find(".note-timestamp").text(timestamp);
+			newNote.find(".note-content").text(content);
+			newNote.find(".video-thumbnail").append($(document.createElement("img")).attr("src", thumbnailUrl));
+			newNote.find(".note-created-at").text(createdAt);
+			newNote.find(".note-tags").text(tags);
+		});
 	});
+
+	function getVideoThumbnailUrl(videoId) {
+		return "https://i1.ytimg.com/vi/" + videoId + "/mqdefault.jpg";
+	}
 })();
