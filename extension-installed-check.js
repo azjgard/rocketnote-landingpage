0fac5cd0dc1b1ef1d789427616ea4eaaca07bcfb
Dustin McCaffree <dustin@smartystreets.com>
2018-02-28 11:39:17 -0700

    Changed off.

--- extension-installed-check.js
+++ extension-installed-check.js
@@ -2,8 +2,8 @@ $(() => {
 		if (document.getElementById('rn_extension-is-installed')) {
 			console.log("Extension is installed!");
 			let inlineInstallButtons = $(".inline-install");
-			inlineInstallButtons.addClass("inline-added");
-			inlineInstallButtons.find("span").text("Added To Chrome").off();
+			inlineInstallButtons.addClass("inline-added").off();
+			inlineInstallButtons.find("span").text("Added To Chrome");
 		}
 	}
 );
