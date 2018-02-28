93f45811fc5f01bc774df69743fb0831bf874494
Dustin McCaffree <dustin@smartystreets.com>
2018-02-28 12:28:27 -0700

    Added universal GTM scripts to all pages.

--- extension-installed-check.js
+++ extension-installed-check.js
@@ -3,7 +3,10 @@ $(() => {
 
 		if (document.getElementById('rn_extension-is-installed')) {
 			inlineInstallButtons.addClass("inline-added");
-			inlineInstallButtons.find("span").text("Added To Chrome");
+			inlineInstallButtons.find("span").text("View Docs");
+			inlineInstallButtons.click(() => {
+
+			});
 		} else {
 			inlineInstallButtons.click(() => {
 				chrome.webstore.install()
