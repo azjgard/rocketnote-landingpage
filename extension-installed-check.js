c0ab97966220277e580e82f472d08b7953142576
Dustin McCaffree <dustin@smartystreets.com>
2018-02-28 11:44:17 -0700

    Turn off inline install if already installed.

--- extension-installed-check.js
+++ extension-installed-check.js
@@ -1,9 +1,13 @@
 $(() => {
+		let inlineInstallButtons = $(".inline-install");
+
 		if (document.getElementById('rn_extension-is-installed')) {
-			console.log("Extension is installed!");
-			let inlineInstallButtons = $(".inline-install");
-			inlineInstallButtons.addClass("inline-added").off();
+			inlineInstallButtons.addClass("inline-added");
 			inlineInstallButtons.find("span").text("Added To Chrome");
+		} else {
+			inlineInstallButtons.click(() => {
+				chrome.webstore.install()
+			});
 		}
 	}
 );
