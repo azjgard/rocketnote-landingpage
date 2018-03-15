5cc5cebf841425cb5d0c0d1e3545256f3d7e4078
Dustin McCaffree <dustin@smartystreets.com>
2018-03-15 09:23:34 -0600

    Log in.

--- extension-installed-check.js
+++ extension-installed-check.js
@@ -4,6 +4,7 @@ $(() => {
 		if (document.getElementById('rn_extension-is-installed')) {
 			inlineInstallButtons.addClass("inline-added");
 			inlineInstallButtons.find("span").text("Added To Chrome");
+			$("#login-button").show();
 		} else {
 			inlineInstallButtons.click(() => {
 				chrome.webstore.install();
