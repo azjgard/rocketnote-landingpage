7b2b0d6a9403b39b3face4b8f9df5e4e84b1f4dc
Dustin McCaffree <dustinnicholasmccaffree@gmail.com>
2018-06-30 10:41:05 -0700

    Added yearly functionality.

--- pricing.js
+++ pricing.js
@@ -6,4 +6,16 @@
     } else {
         console.log("Can't do it, man!");
     }
+
+    $("input[name='selected-service']").change(function(e) {
+        if ($(e.target).attr('id') === 'yearly') {
+            $("#unlimited-price").text("40");
+            $(".duration").text("Yearly");
+            $("#checkout-unlimited").attr("data-product", "531766");
+        } else {
+            $("#unlimited-price").text("5");
+            $(".duration").text("Monthly");
+            $("#checkout-unlimited").attr("data-product", "531765");
+        }
+    });
 })();
